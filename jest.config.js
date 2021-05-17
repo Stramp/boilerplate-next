3d43d1dff002daa94d25676b2734407354e78e4a
rafael <rafael.costa@driven.cx>
2021-05-17 01:16:07 -0300

    intall jest

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.export = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+}
