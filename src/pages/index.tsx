7ef15a3095fdb19260b8be53b1525201b9b01c25
rafael <rafael.costa@driven.cx>
2021-05-21 23:50:04 -0300

    try test fix build next 8

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Header from 'components/Header'
+import Stage from 'components/Stage'
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Stage />
     </>
   )
 }
