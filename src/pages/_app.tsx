fa6f32c7751e9d2a9702cf312808719bac09f2e0
rafael <rafael.costa@driven.cx>
2021-05-21 21:02:41 -0300

    add and config plop generate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle from 'styles/global'
 import theme from 'styles/theme'
@@ -6,6 +7,15 @@ import theme from 'styles/theme'
 function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <title>Boilerplate</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </ThemeProvider>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -30,16 +30,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-BR">
-        <Head>
-          <title>Boilerplate</title>
-          <link rel="shortcut icon" href="/favicon.ico" />
-          <link rel="apple-touch-icon" href="/favicon.ico" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta
-            name="description"
-            content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
-          />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
